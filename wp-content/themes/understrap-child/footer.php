6e216c2e55c7b14f984099cac905c78998de2c1b
jonwhite100 <jon@bpm-web-design.co.uk>
2018-10-17 22:23:13 +0100

    added in static footer

--- wp-content/themes/understrap-child/footer.php
+++ wp-content/themes/understrap-child/footer.php
@@ -13,24 +13,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
-<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
-<footer class="site-footer" id="colophon">
-	<div class="wrapper" id="wrapper-footer">
-		<div class="<?php echo esc_attr( $container ); ?>">
-			<div class="row">
-				<div class="col-md-6">
-					<div class="site-info">
-					    &copy; Copyright Collins Groundworks <?php echo date("Y"); ?>. Site by <a href="https://www.bigplanemedia.co.uk">Big Plane Media</a><span class="sep"></span>
-					</div><!-- .site-info -->
-				</div><!--col end -->
-				<div class="col-md-6 footer-menu">
-					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
-				</div>
-			</div><!-- row end -->
-		</div><!-- container end -->
-	</div><!-- wrapper end -->
-</footer><!-- #colophon -->
-
+	<div class="jon">
+		<?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
+		<footer class="site-footer" id="colophon">
+			<div class="wrapper" id="wrapper-footer">
+				<div class="<?php echo esc_attr( $container ); ?>">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="site-info">
+							    &copy; Copyright Collins Groundworks <?php echo date("Y"); ?>. Site by <a href="https://www.bigplanemedia.co.uk">Big Plane Media</a><span class="sep"></span>
+							</div><!-- .site-info -->
+						</div><!--col end -->
+						<div class="col-md-6 footer-menu">
+							<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+						</div>
+					</div><!-- row end -->
+				</div><!-- container end -->
+			</div><!-- wrapper end -->
+		</footer><!-- #colophon -->
+	</div>
 </div><!-- #page we need this extra closing tag here -->
 <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
 <script>
